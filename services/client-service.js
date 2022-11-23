a035d3aee4e325fca3c46a042311c46d5a4bfa04
jocnn <ijann_@outlook.com>
2022-11-23 12:39:25 -0300

    se creó el contenido de la tabla de forma dinámica

--- services/client-service.js
+++ services/client-service.js
@@ -1,3 +1,33 @@
+const crearNuevaLinea = (nombre, email) => {
+	const linea = document.createElement("tr")
+	const contenido = `
+		<td class="td" data-td>${nombre}</td>
+		<td>${email}</td>
+		<td>
+			<ul class="table__button-control">
+				<li>
+					<a
+						href="../screens/editar_cliente.html"
+						class="simple-button simple-button--edit"
+						>Editar</a>
+				</li>
+				<li>
+					<button
+						class="simple-button simple-button--delete"
+						type="button">
+						Eliminar
+					</button>
+				</li>
+			</ul>
+		</td>`
+
+	linea.innerHTML = contenido
+
+	return linea
+}
+
+const table = document.querySelector("[data-table]")
+
 const http = new XMLHttpRequest()
 
 http.open("GET", "http://localhost:3000/perfil")
@@ -5,6 +35,10 @@ http.open("GET", "http://localhost:3000/perfil")
 http.send()
 
 http.onload = () => {
-	const data = http.response
+	const data = JSON.parse(http.response)
 	console.log(data)
+	data.forEach(perfil => {
+		const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
+		table.appendChild(nuevaLinea)
+	})
 }
