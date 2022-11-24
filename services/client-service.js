2ea49ac3ac4b3c0a415aa7edde622439a9a7fc1b
jocnn <ijann_@outlook.com>
2022-11-24 20:21:52 -0300

    se creó controlador de registro, se uso uuid

--- services/client-service.js
+++ services/client-service.js
@@ -1,5 +1,16 @@
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json())
 
+const crearCliente = (nombre, email) => {
+	return fetch("http://localhost:3000/perfil", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ nombre, email, id: uuid.v4() })
+	})
+}
+
 export const clientServices = {
-	listaClientes
+	listaClientes,
+	crearCliente,
 }
